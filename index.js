4f5636ab46cfc9791d62ee04bc5ea0c356087e56
Reza Fauzan Adhima <rezafauzan593@gmail.com>
2026-01-01 23:48:01 +0700

    Update: add terminal input handler; add navigation to show all food in menu; add navigation from show all food back to main menu

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-async function ambilData(resource){
+async function ambilData(resource) {
     try {
         let rawData = await fetch(resource)
         let dataJson = await rawData.json()
@@ -8,7 +8,38 @@ async function ambilData(resource){
     }
 }
 
-let resource = "https://github.com/rezafauzan/koda-b6-weekly1/raw/refs/heads/master/menu.json"
-
-let menu = ambilData(resource)
-
+let path = "https://github.com/rezafauzan/koda-b6-weekly1/raw/refs/heads/master/menu.json"
+let state = 0
+ambilData(path).then(
+    data => {
+        console.log("==== Selamat Datang di Program Interactive Pembelian Makanan ======")
+        console.log("=== Menu ======")
+        console.log("(1) Lihat list menu makanan")
+        console.log("Silahkan pilih menu dengan memasukan angka")
+        process.stdin.on("data",
+            input => {
+                if(state === 0){
+                    let pilihan = parseInt(input.toString().trim())
+                    if (pilihan === 1) {
+                        data.forEach(
+                            item => {
+                                console.log(`(${item.id}). ${item.nama} ~ ${item.harga}`)
+                            }
+                        )
+                        console.log("(0). Untuk kembali ke halaman utama")
+                        state = 1
+                    }
+                }else if(state === 1){
+                    let pilihan = parseInt(input.toString().trim())
+                    if (pilihan === 0) {
+                        console.log("==== Selamat Datang di Program Interactive Pembelian Makanan ======")
+                        console.log("=== Menu ======")
+                        console.log("(1) Lihat list menu makanan")
+                        console.log("Silahkan pilih menu dengan memasukan angka")
+                        state = 0
+                    }
+                }
+            }
+        )
+    }
+)
