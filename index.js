46489a8dcd2311b7b007fb7cb95c1a926172d20e
Reza Fauzan Adhima <rezafauzan593@gmail.com>
2026-01-04 03:14:17 +0700

    Update: Fix some navigation issues;add cart feature now in food detail there is option to add it to cart

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ async function ambilData(resource) {
 
 let path = "https://github.com/rezafauzan/koda-b6-weekly1/raw/refs/heads/master/menu.json"
 let state = 0
+let choice
+let cart = []
 
 // state 0 menu utama
 // state 0 pilihan 1 list makanan
@@ -30,7 +32,7 @@ ambilData(path).then(
         console.log("Silahkan pilih menu dengan memasukan angka")
         process.stdin.on("data",
             input => {
-                if(state === 0){
+                if (state === 0) {
                     let pilihan = parseInt(input.toString().trim())
                     if (pilihan === 1) {
                         data.forEach(
@@ -43,35 +45,64 @@ ambilData(path).then(
                         state = 1
                     }
                 }
-                else if(state === 1){
+                else if (state === 1) {
                     let pilihan = input.toString().trim()
-                    if( !isNaN(pilihan) ){
+                    if (!isNaN(pilihan)) {
+                        pilihan = parseInt(pilihan)
                         if (pilihan === 0) {
                             console.log("==== Selamat Datang di Program Interactive Pembelian Makanan ======")
                             console.log("=== Menu ======")
                             console.log("(1) Lihat list menu makanan")
                             console.log("Silahkan pilih menu dengan memasukan angka")
-                            state = 9
-                        }else{
-                            console.log(`Nama Makanan :${data[parseInt(pilihan)-1].nama} `)
-                            console.log(`Harga per sajian :${data[parseInt(pilihan)-1].harga} `)
-
+                            state = 0
+                        } else {
+                            console.log(`Nama Makanan :${data[parseInt(pilihan) - 1].nama} `)
+                            console.log(`Harga per sajian :${data[parseInt(pilihan) - 1].harga} `)
+                            console.log("(+). Untuk menambahkan ke keranjang")
+                            console.log("(0). Untuk kembali ke list makanan")
+                            choice = data[parseInt(pilihan) - 1]
+                            state = 2
                         }
-                    }else{
+                    } else {
                         pilihan = input.toString().trim()
-                        let filtered = searcher(pilihan,data)
+                        let filtered = searcher(pilihan, data)
                         filtered.forEach(
                             item => {
                                 console.log(`(${item.id}). ${item.nama} ~ Rp.${item.harga},-`)
                             }
                         )
                         state = 1
-                        console.log("(0). Untuk kembali ke halaman utama")
                     }
                 }
-                else if(state === 9){
+                else if (state === 2) {
+                    let pilihan = input.toString().trim()
+                    if (!isNaN(pilihan)) {
+                        pilihan = parseInt(pilihan)
+                        if (pilihan === 0) {
+                            console.log(pilihan)
+                            data.forEach(
+                                item => {
+                                    console.log(`(${item.id}). ${item.nama} ~ Rp.${item.harga},-`)
+                                }
+                            )
+                            console.log("()Masukan kata untuk mencari menu")
+                            console.log("(0). Untuk kembali ke halaman utama")
+                            state = 1
+                            // console.log("ke state 1")
+                        }
+                    } else {
+                        if(pilihan === "+"){
+                            // console.log(pilihan)
+                            // console.log(choice)
+                            cart.push(choice)
+                            console.log("Menu ini dimasukan ke keranjang")
+                        }
+                    }
+                }
+                else if (state === 9) {
                     let pilihan = parseInt(input.toString().trim())
                     if (pilihan === 0) {
+                        console.log("State 9")
                         console.log("==== Selamat Datang di Program Interactive Pembelian Makanan ======")
                         console.log("=== Menu ======")
                         console.log("(1) Lihat list menu makanan")
